69adb1b728e1f1a571bda9678f02adaec8135bed
amrutha10499 <amrutha.xworkz@gmail.com>
2021-10-25 10:04:55 +0530

    vari

---
+++ variables/Array.java
@@ -0,0 +1,8 @@
+class Array1{
+  static byte[] numbers={1,2,3,4,5};
+	public static void main(String[]args){
+		int prime_number[]={2,3,5,7,11};
+		System.out.print(prime_number[1]);
+		System.out.print(numbers[2]);
+	}
+}
